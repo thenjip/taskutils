e1886c8285f199db1e6f2bba4ae24a6de841c0f5
thenjip <39303840+thenjip@users.noreply.github.com>
2020-11-01 14:25:32 +0100

    Fix #19
    Rename 'longOptionPrefix()' to 'nimLongOptionPrefix'().

--- src/taskutils/nimcmdline.nim
+++ src/taskutils/nimcmdline.nim
@@ -8,24 +8,58 @@ type
 
 
 
-func longOptionPrefix* (): string =
+func nimLongOptionPrefix* (): string =
+  ##[
+    Since 0.2.0.
+  ]##
   "--"
 
 
-func longOption* (name: OptionName): string =
-  fmt"{longOptionPrefix()}{name}"
+func longOptionPrefix* (): string {.deprecated.} =
+  ##[
+    Deprecated since 0.2.0.
+    Use `nimLongOptionPrefix` instead.
+  ]##
+  nimLongOptionPrefix()
 
 
-func longOption* (name: OptionName, value: OptionValue): string =
-  fmt"{name.longOption()}:{value.quoteShell()}"
+
+func nimLongOption* (name: OptionName): string =
+  ##[
+    Since 0.2.0.
+  ]##
+  fmt"{nimLongOptionPrefix()}{name}"
+
+
+func longOption* (name: OptionName): string {.deprecated.} =
+  ##[
+    Deprecated since 0.2.0.
+    Use `nimLongOption` instead.
+  ]##
+  name.nimLongOption()
+
+
+func nimLongOption* (name: OptionName, value: OptionValue): string =
+  ##[
+    Since 0.2.0.
+  ]##
+  fmt"{name.nimLongOption()}:{value.quoteShell()}"
+
+
+func longOption* (name: OptionName, value: OptionValue): string {.deprecated.} =
+  ##[
+    Deprecated since 0.2.0.
+    Use `nimLongOption` instead.
+  ]##
+  name.nimLongOption(value)
 
 
 
 func toNimLongOptions* (options: varargs[OptionName]): seq[string] =
-  options.map(name => name.longOption())
+  options.map(name => name.nimLongOption())
 
 
 func toNimLongOptions* (
   options: varargs[tuple[name: OptionName, value: OptionValue]]
 ): seq[string] =
-  options.map(opt => opt.name.longOption(opt.value))
+  options.map(opt => opt.name.nimLongOption(opt.value))
